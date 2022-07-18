7c07a6dd88a31d8c8eb33a47d6431623f38b72cc
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-18 12:09:35 +0000

    [Add] 4-25：コントローラ作成=>books,users／,Viewファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  # get 'homes/top'
+  root to: "homes#top"
+
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+
+  get 'users/show'
+  get 'users/edit'
+
 end
