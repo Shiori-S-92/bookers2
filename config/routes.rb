b2cd5c41a7f512856d07c00af7ae9285461983ef
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-23 13:37:02 +0000

    [Update] 4-25：レイアウト調整_ヘッダー／[Add]users => index

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   devise_for :users
-  # get 'homes/top'
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
 
-  # get 'books/new'
-  # get 'books/index'
-  # get 'books/show'
-  # get 'books/edit'
   resources :books, only: [:new, :index, :show]
 
-  # get 'users/show'
-  # get 'users/edit'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:index, :show, :edit]
 
 end
