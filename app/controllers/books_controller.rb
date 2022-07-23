22f91a0893e1e34eceabafa8ed516f0e657896cc
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-23 15:27:32 +0000

    [Update] 4-25：『bookのindexページの左側の作成(User info)』途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,17 @@
 class BooksController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
   end
 
   def index
+    @books = Book.page(params[:page])
   end
 
   def show
@@ -10,4 +19,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
 
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :index, :show, :create]
 
   resources :users, only: [:index, :show, :edit]
 
